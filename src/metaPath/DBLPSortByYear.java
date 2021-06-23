3710da2111fd23d76b18d9b547c6ef5a998a8b47
Emma-ning <74041707+Emma-ning@users.noreply.github.com>
2021-06-23 17:31:41 +0800

    Update DBLPSortByYear.java

--- src/metaPath/DBLPSortByYear.java
+++ src/metaPath/DBLPSortByYear.java
@@ -30,8 +30,8 @@ public class DBLPSortByYear {
 	
     public static void sortByYear(YearPaperInex[] paper_year_array) {
         Arrays.sort(paper_year_array, new Comparator<YearPaperInex>() {
-            public int compare(YearPaperInex p1, YearPaperInex p2) {
-                return p1.year.compareTo(p2.year);
+            public int compare(YearPaperInex p1, YearPaperInex p2) { 
+                return p1.year.compareTo(p2.year);                                  //对两个数组进行比较
             }
         });
     }
@@ -51,16 +51,16 @@ public class DBLPSortByYear {
 				StringTokenizer st = new StringTokenizer(currentLineString,"\t");  
 				paperIndex = st.nextToken();
 				year = st.nextToken();
-				paper_year.add(new YearPaperInex(year, paperIndex));
+				paper_year.add(new YearPaperInex(year, paperIndex));              //读取文件paper_year.txt，添加到paper_year这个list中
 			}
 
 			
-	    	YearPaperInex[] paper_year_array = new YearPaperInex[paper_year.size()];
-	    	paper_year_array = paper_year.toArray(paper_year_array);
+	    	YearPaperInex[] paper_year_array = new YearPaperInex[paper_year.size()];  //使得paper_year_array和paper_year有同样的size
+	    	paper_year_array = paper_year.toArray(paper_year_array);   //toArray()方法以正确的顺序(从第一个到最后一个元素)返回一个包含此列表中所有元素的数组。
 	    	
-			sortByYear(paper_year_array);
+			sortByYear(paper_year_array);   //调用了sortByYear对这个array进行处理
 			int count = 0;
-	        for (YearPaperInex p : paper_year_array) {
+	        for (YearPaperInex p : paper_year_array) {                      //对于paper_year_array中的每一行，分别写入文件paper_year_sorted.txt中
 	        	count++;
 	        	if (count%(3272992/2)==0)
 	        		System.out.println(p.year);
